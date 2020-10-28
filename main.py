234daadd52f88db88985631ec6531465d9ca121a
Sebastian Korniewicz <sebak19962@gmail.com>
2020-10-28 13:58:28 +0100

    update yolov3 init function, test YOLOv3-416

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from detectors.detector import ExampleDetector
 from detectors.detector_ssd_mobile import DetectorSSDMobileNetv2
 from detectors.yolo_v3 import Yolov3
 
-detector = Yolov3(threshold=0.1)
+detector = Yolov3(threshold=0.1,
+                  model_weights="https://pjreddie.com/media/files/yolov3.weights",
+                  model_cfg="https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg")
 video = Video('data/sherbrooke_video.avi', detector)
 video.analyze()
