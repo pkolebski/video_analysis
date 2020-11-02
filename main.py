c0549df362fd8408959e118240b4686c4a9ec829
Sebastian Korniewicz <sebak19962@gmail.com>
2020-11-02 16:21:38 +0100

    add non-maximum suppression - yolov3

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from detectors.detector import ExampleDetector
 from detectors.detector_ssd_mobile import DetectorSSDMobileNetv2
 from detectors.yolo_v3 import Yolov3
 
-detector = Yolov3(threshold=0.1,
-                  model_weights="https://pjreddie.com/media/files/yolov3.weights",
+detector = Yolov3(model_weights="https://pjreddie.com/media/files/yolov3.weights",
                   model_cfg="https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg")
-video = Video('data/sherbrooke_video.avi', detector)
+video = Video('data/stmarc_video.avi', detector)
 video.analyze()
